4cce1b09acb84732156dfe3775389901ac442183
kakaw <107406703+brunakakaw@users.noreply.github.com>
2024-04-16 11:25:09 -0300

    .1

--- main.js
+++ main.js
@@ -47,7 +47,14 @@ function calculaTempo(tempoObjetivo){
     } else {
         return [0,0,0,0];
     }
-   
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+
+    segundos %=60;
+    
 }
 
 
